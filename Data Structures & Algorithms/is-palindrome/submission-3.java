174865e416590eca45de2ed17bdb7262a778cac3
praneshragavendar <60648759+praneshragavendar@users.noreply.github.com>
2026-08-26 10:11:41 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAlphaNum(char ch){
+        return (ch>='A'&&ch<='Z'||ch>='a'&&ch<='z'||ch>='0'&&ch<='9');
+    }
+    public boolean isPalindrome(String s) {
+        int i=0,j=s.length()-1;
+        while(i<j){
+            while(i<j&&!isAlphaNum(s.charAt(i))){
+                i++;
+            }
+            while(i<j&&!isAlphaNum(s.charAt(j))){
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+            j--;
+            i++;
+        }
+        return true;
+    }
+}
